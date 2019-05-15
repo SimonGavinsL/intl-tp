7c5e35c4e54e794b7532c9d59af2e3ff91a72dfc
simongavinslee <simongavinslee0013@icloud.com>
2019-05-15 15:18:51 +0800

    Ant-design setup

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Button from 'ant-design-vue/lib/button'
+import 'ant-design-vue/dist/antd.css'
+Vue.component(Button.name, Button)
+
 Vue.config.productionTip = false
 
 new Vue({
